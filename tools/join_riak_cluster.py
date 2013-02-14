21f9cc93807db299072dc33ee513083070dc0e88
Andrew Matheny <andrew.matheny@listhub.com>
2013-02-14 16:03:32 -0500

    Stop passing around the riak_ips array since it's not used anywhere

--- tools/join_riak_cluster.py
+++ tools/join_riak_cluster.py
@@ -17,12 +17,12 @@ def get_group_info(ec2conn, instance_id) :
     reservations = ec2conn.get_all_instances(instance_id)
     instance = [i for r in reservations for i in r.instances][0]
     if instance.tags.has_key('aws:autoscaling:groupName'):
-        private_ip, first_node, riak_ips, nodes_total, stack_id, node_number, instance = get_autoscale_info(ec2conn, instance_id)
+        private_ip, first_node, nodes_total, stack_id, node_number, instance = get_autoscale_info(ec2conn, instance_id)
     elif instance.tags.has_key('stackId'):
-        private_ip, first_node, riak_ips, nodes_total, stack_id, node_number, instance = get_reservation_info(ec2conn, instance_id)
+        private_ip, first_node, nodes_total, stack_id, node_number, instance = get_reservation_info(ec2conn, instance_id)
     else:
         return None
-    return private_ip, first_node, riak_ips, nodes_total, stack_id, node_number, instance
+    return private_ip, first_node, nodes_total, stack_id, node_number, instance
 
 def get_autoscale_info(ec2conn, instance_id) :
 
@@ -39,7 +39,7 @@ def get_autoscale_info(ec2conn, instance_id) :
     riak_ips.sort()
     first_node = riak_ips[0]
     node_number = riak_ips.index(private_ip) + 1
-    return private_ip, first_node, riak_ips, len(riak_ips), autoscale_group, node_number, instance
+    return private_ip, first_node, len(riak_ips), autoscale_group, node_number, instance
 
 
 def get_reservation_info(ec2conn, instance_id) :
@@ -69,7 +69,7 @@ def get_reservation_info(ec2conn, instance_id) :
     instances = [i for r in reservations for i in r.instances]
     for instance in instances:
         riak_ips.append(instance.private_ip_address)
-    return private_ip, first_node, riak_ips, int(nodes_total), stack_id, node_number, instance
+    return private_ip, first_node, int(nodes_total), stack_id, node_number, instance
 
 def plan_commit(total_nodes):
     ready = False
@@ -109,7 +109,7 @@ region_name = zone[:-1]
 
 print "Connecting to region %s" % region_name
 ec2conn = ec2.connect_to_region(region_name)
-private_ip, first_node, riak_ips, nodes_total, stack_id, node_number, instance = get_group_info(ec2conn, instance_id)
+private_ip, first_node, nodes_total, stack_id, node_number, instance = get_group_info(ec2conn, instance_id)
 
 print "Instance belongs to %s. Finding first node" % stack_id
 print "Node number for this machine is %s." % node_number
